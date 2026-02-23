ee3e2f30f6cc72abea4a3cd68137cec51828a62f
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-02-23 10:27:49 -0500

    fixed typo M doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package config provides a simple API for reading
+// Package conflux provides a simple API for reading
 // configs from the environment, yaml files, and
 // bitwarden secrets
 package conflux
